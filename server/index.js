234f7b12563c991b92a4e214ee68d9e7d8858dea
yurcifer <yurcifer.dev@gmail.com>
2022-08-02 19:00:39 +0300

    added main dependencies. Init main function

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+
+const app = express();
+const PORT = config.get('serverPort');
+
+const start = async () => {
+  try {
+    await mongoose.connect(config.get('dbUrl'));
+    app.listen(PORT, () => {
+      console.log('Server started on port ', PORT);
+    })
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
