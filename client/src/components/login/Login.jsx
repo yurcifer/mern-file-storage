97abbc84972cfb0bf88f5012f96304091669524d
yurcifer <yurcifer.dev@gmail.com>
2022-08-09 12:03:52 +0300

    made forms controlled

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../utils/input/input';
 import LoginBtn from '../../utils/loginBtn/LoginBtn';
 import styles from './login.module.scss';
 
 function Login() {
-    return (
-      <div className={styles.container}>
-        <h3>Login</h3>
-        <Input type="text" placeholder="Введите email..." />
-        <Input type="password" placeholder="Введите пароль..." />
-        <LoginBtn className={styles.btn} >Sign in</LoginBtn>
-      </div>
-    );
-  }
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const clickHandler = () => {
+    setLogin('');
+    setPassword('');
+  };
+
+  return (
+    <div className={styles.container}>
+      <h3>Login</h3>
+      <Input
+        type="text"
+        placeholder="Введите email..."
+        value={login}
+        onChange={e => setLogin(e.target.value)}
+      />
+      <Input
+        type="password"
+        placeholder="Введите пароль..."
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <LoginBtn onClick={clickHandler} className={styles.btn}>
+        Sign in
+      </LoginBtn>
+    </div>
+  );
+}
 
 export default Login;
--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './registration.module.scss';
 import Input from '../../utils/input/input';
 import LoginBtn from '../../utils/loginBtn/LoginBtn';
 
 function Registration() {
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+
+  const clickHandler = () => {
+    setLogin('');
+    setPassword('');
+  };
   return (
     <div className={styles.container}>
       <h3>Registration</h3>
-      <Input type="text" placeholder="Введите email..." />
-      <Input type="password" placeholder="Придумайте надёжный пароль..." />
-      <LoginBtn className={styles.btn}>Sign up</LoginBtn>
+      <Input
+        type="text"
+        placeholder="Введите email..."
+        value={login}
+        onChange={e => setLogin(e.target.value)}
+      />
+      <Input
+        type="password"
+        placeholder="Придумайте надёжный пароль..."
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <LoginBtn onClick={clickHandler} className={styles.btn}>
+        Sign up
+      </LoginBtn>
     </div>
   );
 }
