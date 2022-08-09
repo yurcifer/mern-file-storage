598a1759c6e786adc030596a3ce10383ee463bdc
yurcifer <yurcifer.dev@gmail.com>
2022-08-09 18:06:29 +0300

    added cors, fixed error response

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('config');
 const authRouter = require('./routes/auth.routes');
+const cors = require('./middleware/cors.middleware');
 
 const app = express();
 const PORT = config.get('serverPort');
 
+app.use(cors);
 app.use(express.json());
 app.use('/api/auth', authRouter);
 const start = async () => {
@@ -13,7 +15,7 @@ const start = async () => {
     await mongoose.connect(config.get('dbUrl'));
     app.listen(PORT, () => {
       console.log('Server started on port ', PORT);
-    })
+    });
   } catch (e) {
     console.log(e);
   }
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,7 +2,7 @@ const Router = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const config = require('config')
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 const router = new Router();
 
@@ -22,7 +22,7 @@ router.post(
       const candidate = await User.findOne({ email });
 
       if (candidate) {
-        return res.status(400).json({ message: `User with email ${email} already exists` });
+        return res.status(400).send({ message: `User with email ${email} already exists` });
       }
       const hashPassword = await bcrypt.hash(password, 7);
       const user = new User({ email, password: hashPassword });
@@ -34,37 +34,34 @@ router.post(
   }
 );
 
-router.post(
-  '/login',
-  async (req, res) => {
-    try {
-      const { email, password } = req.body;
-
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(400).json({ message: `User with email ${email} doesn't exist` });
-      }
-
-      const isPassValid = await bcrypt.compare(password, user.password);
-      if (!isPassValid) {
-        return res.status(400).json({ message: 'invalid password' });
-      }
-
-      const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: '5d'});
-      return res.json({
-        token,
-        user: {
-          id: user.id,
-          email: user.email,
-          diskSpace: user.diskSpace,
-          usedSpace: user.usedSpace,
-          avatar: user.avatar
-        }
-      })
-    } catch (e) {
-      console.log(e);
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    console.log(email);
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send({ message: `User with email ${email} doesn't exist` });
     }
+
+    const isPassValid = await bcrypt.compare(password, user.password);
+    if (!isPassValid) {
+      return res.status(400).send({ message: 'invalid password' });
+    }
+
+    const token = jwt.sign({ id: user.id }, config.get('secretKey'), { expiresIn: '5d' });
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar,
+      },
+    });
+  } catch (e) {
+    console.log(e);
   }
-);
+});
 
 module.exports = router;
